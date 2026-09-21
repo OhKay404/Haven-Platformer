24c0af2147a9dc2294def8c6ce7eeaa35a72e107
OhKay404 <kayleechan0322@gmail.com>
2026-09-20 22:39:53 -0400

    Add text to tell player that they win after 3 coins are collected

--- main.gd
+++ main.gd
@@ -2,6 +2,7 @@ extends Node
 
 var num_coins:int = 0
 @onready var coin_text:RichTextLabel = $CoinsCollected
+@onready var win_text:RichTextLabel = $Winner
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	coin_text.text = "Coins: " + str(num_coins) + "/3"
+	if num_coins == 3:
+		win_text.modulate = Color("#ffffff")
+		get_tree().paused = true
